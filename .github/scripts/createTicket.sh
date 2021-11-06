859fd98d670670a41796a0a3f001b38d81468ed4
kz2d <56164149+kz2d@users.noreply.github.com>
2021-11-06 22:12:40 +0300

    fixed

--- .github/scripts/createTicket.sh
+++ .github/scripts/createTicket.sh
@@ -4,7 +4,7 @@ LAST_TAG=$(git tag -l | tac | head -n1)
 PREV_TAG=$(git tag -l | tac |tail -n +2| head -n1  )  
 AUTHOR=$(git show -s --format='%an' ${LAST_TAG})
 DATE=$(git show -s --format='%cd' ${LAST_TAG})
-CHANGELOG=$(git log ${PREV_TAG}..${LAST_TAG} --pretty=format:"%h - %s, %ad\n" )
+CHANGELOG=$(git log ${PREV_TAG}..${LAST_TAG} --pretty=format:"%h - %s, %ad\n"  | tr -s "\n" " " )
 SUMMARY="Realis for ${LAST_TAG} in "
 UNIQUE="$REPO_NAME/$LAST_TAG"
 #TODO ${CHANGELOG}
